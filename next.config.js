bdff309b8790171006f05cc33a1abddfb462e187
nekikara <nekikara@gmail.com>
2019-05-26 00:17:35 +0200

    Stop using nextjs

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const withTypescript = require('@zeit/next-typescript');
-const debug = process.env.NODE_ENV !== "production";
-module.exports = withTypescript({
-    assetPrefix: !debug ? 'https://nekikara.github.io/react-code-samples/' : '',
-});
